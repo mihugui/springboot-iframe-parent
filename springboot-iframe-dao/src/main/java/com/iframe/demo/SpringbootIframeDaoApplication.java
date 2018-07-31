45c65181b13ef75fa0da5429f451cdb5fe88f610
潘文涛 <pwt980408@hotmail.com>
2018-07-31 16:29:59 +0800

    项目上传

---
+++ springboot-iframe-dao/src/main/java/com/iframe/demo/SpringbootIframeDaoApplication.java
@@ -0,0 +1,12 @@
+package com.iframe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIframeDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIframeDaoApplication.class, args);
+    }
+}
---
+++ springboot-iframe-web/src/main/java/com/iframe/demo/SpringbootIframeWebApplication.java
@@ -0,0 +1,12 @@
+package com.iframe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIframeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIframeWebApplication.class, args);
+    }
+}
